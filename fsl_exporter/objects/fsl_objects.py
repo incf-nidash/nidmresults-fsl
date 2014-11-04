0df41d0636a16018092670d30dded615d6ca0332
cmaumet <c.m.j.maumet@warwick.ac.uk>
2014-11-04 09:42:46 +0000

    add software version

--- fsl_exporter/objects/fsl_objects.py
+++ fsl_exporter/objects/fsl_objects.py
@@ -6,6 +6,9 @@ FSL-specific classes and classes overloaded to add FSL-specific attributes.
 """
 from exporter.objects.generic import NIDMObject
 from exporter.objects.constants import *
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class Software(NIDMObject):
     # FIXME software should be generic and then overloaded
@@ -19,6 +22,28 @@ class Software(NIDMObject):
         self.feat_version = feat_version
         self.name = "FSL"
         self.id = NIIRI['software_id']
+        # Retreive FSL version from feat version 
+        # (cf. https://github.com/incf-nidash/nidm-results_fsl/issues/3)
+        if feat_version == "6.00":
+            self.version = "fsl-5_0_x"
+        elif feat_version == "5.98":
+            self.version = "fsl-4_1_x"
+        elif feat_version == "5.92":
+            self.version = "fsl-4_0_x"
+        elif feat_version == "5.91":
+            self.version = "fsl-4_0_1"
+        elif feat_version == "5.90":
+            self.version = "fsl-4_0"
+        elif feat_version == "5.61":
+            self.version = "fsl-3_3"
+        elif feat_version == "5.4":
+            self.version = "fsl-3_2_1"
+        elif feat_version == "5.1":
+            self.version = "fsl-3_1_1_x"
+        else:
+            logging.debug("FSL version unknow for feat version: \""+\
+                feat_version+"\"")
+            self.version = "fsl-unknown"
 
     def export(self):
         """
@@ -28,6 +53,7 @@ class Software(NIDMObject):
             other_attributes=((PROV['type'], NIDM[self.name]), 
                             (PROV['type'], PROV['SoftwareAgent']),
                             (PROV['label'],self.name),
+                            (NIDM['softwareVersion'],self.version),
                             (FSL['featVersion'], self.feat_version)))
 
 
