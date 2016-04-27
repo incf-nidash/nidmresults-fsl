5a82e58c14bedccd4875d6d4e2061b537d9e3fca
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-04-27 12:27:30 +0100

    nidmfsl now relies on nidmresults 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = list(filter(None, reqs))
 
 setup(
     name="nidmfsl",
-    version="0.2.0",
+    version="0.3.0",
     author="Camille Maumet",
     author_email="c.m.j.maumet@warwick.ac.uk",
     description=("Export of FSL statistical results using NIDM\
