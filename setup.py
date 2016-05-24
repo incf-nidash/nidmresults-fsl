b4169169886a74ae8b3e8fb99be3eb69406008d7
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-05-24 11:14:50 +0100

    version 0.3.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = list(filter(None, reqs))
 
 setup(
     name="nidmfsl",
-    version="dev",
+    version="0.3.3",
     author="Camille Maumet",
     author_email="c.m.j.maumet@warwick.ac.uk",
     description=("Export of FSL statistical results using NIDM\
